3d09ee9af569e7dddc6a5af17f8ba6c9a1b3f532
Dilshanpadukka <udayankadilshan23@gmail.com>
2024-06-26 15:36:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+let randomNumber = Math.floor((Math.random()*10)+1);
+console.log(randomNumber);
+
+let lblOutput = document.getElementById("output");
+
+function btnGuess() {
+    let guess = document.getElementById("guessNumber").value;
+    let random = randomNumber;
+    if (guess>10) {
+    console.log("Try Again...Your Number is Invalid...");
+    lblOutput.innerHTML = "Try Again...Your Number is Invalid...";
+    }else if (random==guess) {
+        console.log("You win...Your guessing Number is Correct");
+        lblOutput.innerHTML = "You win...Your guessing Number is Correct";
+    }else if (random>guess) {
+        console.log("Try Again...Your Guessing Number is Too low");
+        lblOutput.innerHTML = "Try Again...Your Guessing Number is Too low";
+    }else if (random<guess) {
+        console.log("Try Again...Your Guessing Number is Too High");
+        lblOutput.innerHTML = "Try Again...Your Guessing Number is Too High";
+    }else if (guess>10) {
+        console.log("Try Again...Your Number is Invalid...");
+        lblOutput.innerHTML = "Try Again...Your Number is Invalid...";
+    }
+    
+}
